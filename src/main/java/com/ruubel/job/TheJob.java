7f42aa12d876b38df46a2f9e9cf8377ebb97ce3b
Indrek Ruubel <indrekruubel@gmail.com>
2018-09-15 15:38:25 +0300

    enable scheduling

--- src/main/java/com/ruubel/job/TheJob.java
+++ src/main/java/com/ruubel/job/TheJob.java
@@ -13,6 +13,7 @@ import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -39,7 +40,7 @@ public class TheJob {
         this.mailService = mailService;
     }
 
-    //    @Scheduled(cron = "0 0/30 * * * ?") // Every 30 minutes
+    @Scheduled(cron = "0 0/30 * * * ?") // Every 30 minutes
     public void run() {
         log.info("Running job");
         Document document = getDocument(url);

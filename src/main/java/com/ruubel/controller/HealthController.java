1a8d0ab1e2379e2b3f3da2e00eeb8fbe89299f9e
Indrek Ruubel <indrekruubel@gmail.com>
2018-09-15 15:45:09 +0300

    health endpoint

---
+++ src/main/java/com/ruubel/controller/HealthController.java
@@ -0,0 +1,22 @@
+package com.ruubel.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/health")
+public class HealthController {
+
+    @GetMapping
+    public ResponseEntity<Map<String, Object>> health() {
+        return new ResponseEntity(new HashMap<String, Object>(){{
+            put("alive", true);
+        }}, HttpStatus.OK);
+    }
+}
